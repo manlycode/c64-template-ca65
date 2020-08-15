c07ec40ebcefae367da39db024b60dd3b7738bea
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2020-08-15 12:06:50 -0400

    Automatically run tests.

--- bin/c64-debugger.sh
+++ bin/c64-debugger.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 debuggerBin="/Applications/C64Debugger.app/Contents/MacOS/C64Debugger -pass"
-$debuggerBin -clearsettings -snapshot clean-c64.snap -symbols $2 -prg $1 -breakpoints $3 -watch $4
+$debuggerBin -clearsettings -autojmp -snapshot clean-c64.snap -symbols $2 -prg $1 -breakpoints $3 -watch $4 

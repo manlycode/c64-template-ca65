e5df068ab71bcc213b81721f274f59c827049d89
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2020-08-19 16:38:24 -0400

    Successfully print screen.

--- src/map.asm
+++ src/map.asm
@@ -1,26 +1,31 @@
+screenW = 40
+screenH = 25
+
+mapIdx: .byte $00
+screenIdx: .byte $00
+vpX: .byte $00
+vpY: .byte $00
 
 .scope map_module
 ;------------------------------------------------------------------
 ; Variables
 ;------------------------------------------------------------------
 
-mapIdx: .byte $00
-screenIdx: .byte $00
-mapPtr:    .addr $0000
-screenPtr: .addr $0000
-
-.export mapPtr, screenPtr
-
 
 ;------------------------------------------------------------------
 ; Subroutines
 ;------------------------------------------------------------------
-.macro mapInitChars Chars
-        chars_ADDR = Chars
-.endmacro
+.macro copyChars source,target,size
+        ldx #0
+:       
+.repeat 8
+        lda source,x
+        sta target,x
+.endrepeat
+        inx
+        bne :-
 
-.macro mapInitColors Colors
-        colors_ADDR = Colors
+
 .endmacro
 
 .macro loadChar Idx
@@ -32,5 +37,53 @@ screenPtr: .addr $0000
         ldx Idx
         lda colors_ADDR,x
 .endmacro
+
+.macro copyMap Map, mapW, mapH, tileW, tileH, Charset, charCOUNT, Colors, colorCOUNT, Screen
+        lda #0
+        sta mapIdx
+        sta screenIdx
+
+@loop:
+        ldy screenIdx
+        ldx mapIdx
+
+        .repeat (25 / tileH), I
+        lda Map+((mapW*tileH)*I),x
+        sta Screen+((screenW*tileH)*I),y
+        .endrepeat
+
+        inc mapIdx
+        clc
+        inc screenIdx
+        lda screenIdx
+        cmp #40
+        beq @end
+        beq :+
+        cmp #80
+        beq :+
+        cmp #120
+        beq :+
+        cmp #160
+        beq :+
+        cmp #200
+        beq :+
+        cmp #240
+        beq :+
+        ; Not a screen bounds, normal map index inc
+        jmp @loop
+        
+:       ; forward the map index
+        clc
+        lda mapIdx
+        adc #(screenW .mod mapW)
+        sta mapIdx
+        jmp @loop
+
+@end:
+
+
+
+.endmacro
+
 
 .endscope
--- tests/test-cases/map/test-init-map.asm
+++ tests/test-cases/map/test-init-map.asm
@@ -1,14 +1,22 @@
 ; @access public
 ; @return void
 testInitMap: .scope
-        ; screen = $3400
-        ; charsTarget = $3800 
-        ; copyChars charset, charsTarget, #CHARSET_COUNT
-
+        copyMap map, MAP_WIDTH, MAP_HEIGHT, 1, 1, charset, CHARSET_COUNT, colors, COLORS_COUNT, targetScreen
         rts
+
+viewPort:
+        .byte $01
+        .byte $02
+
+.segment "SCREEN"
+targetScreen:
+        .repeat 1000,I
+                .byte $00
+        .endrepeat 
 .segment "MAP"
         .include "tests/assets/commando-map.s"
-.segment "HIRAM"
+.segment "CHAR"
         .include "tests/assets/commando-charset.s"
+.segment "HIRAM"
         .include "tests/assets/commando-colors.s"
 .endscope
